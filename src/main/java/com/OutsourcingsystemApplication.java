ae6499099778a233d2bc81b242a66f782e988423
zhang_ocean <1125694337@qq.com>
2018-01-11 21:42:38 +0800

    OutsourcingSystem 1.0

---
+++ src/main/java/com/OutsourcingsystemApplication.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsourcingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutsourcingsystemApplication.class, args);
+	}
+}
