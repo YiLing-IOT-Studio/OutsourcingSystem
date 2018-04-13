90858b28ce606d76fbe9cf1523dcddb7ac90eb73
杨玉卿 <1453582072@qq.com>
2018-04-13 16:32:35 +0800

    yyq 2018.4.13 任务发布

--- src/main/resources/static/js/addNode.js
+++ src/main/resources/static/js/addNode.js
@@ -5,129 +5,70 @@
 var oBtn1=$(".addAll");
 var oTask=$("#task");
 oBtn1.click(function(){
+    oTask.html("");
     var oDiv=$('<div class="oneTask page2 col-md-offset-2 col-md-8"></div>');
     //taskTitle
-    var oP1=$("<p class='text-center taskTitle'></p>");
+    var oP1=$("<p class=' taskTitle'></p>");
     oP1.append("<label>项目名</label>"+"<br/>"+
-        "<input type='text' class='taskName'/>"+"<br/>");
+        "<input type='text' class='projectName'/>"+"<br/>");
     oDiv.append(oP1);
-
-    //taskContent
+    //taskName
+    var oP=$("<p class='text-left'></p>");
+    oP.append('<label>任务名</label>'+'<br/>'+ "<input type='text' class='taskName'/>"+"<br/>");
+    oDiv.append(oP);
+    //task
     var oP2=$("<p class='task'></p>");
-    oP2.append("<label>任务总述</label>"+"</br>"+
-                 "<textarea placeholder='任务总述' class='taskContent'></textarea>"+"<br/>"
-              +"<div>"+"<label>子任务</label>"+"<br/>"+
+    oP2.append("<div class='taskSon'>"+"<label>任务</label>"+"<br/>"+
                "<textarea placeholder='任务描述' class='taskDev'></textarea>"+"<br/>"
-              + "<div class='text-right'>"+ "<label>任务负责人</label>"+"<input type='text' class='staffName'/>"+"</div>"
+              + "<div class='text-right task-grade'>"+ "<span class='grade'>任务等级&nbsp;&nbsp;</span>"+"<label class='radio-inline'>"+
+                "<input type='radio' value='低' name='authority'/>"+"低</label>"+"<label class='radio-inline'>"+
+                "<input type='radio' value='中' name='authority'/>"+"中</label>"+"<label class='radio-inline'>"+
+                "<input type='radio' value='高' name='authority'/>"+"高</label>"+"<br/>"
               +"</div>"
     );
     oDiv.append(oP2);
-
-    //taskBtn
-    var oP3=$("<p class='text-right'></p>");
-    oP3.append("<button type='button' class='btn btn-default btn-sm addTask'>" +
-        "<span class='glyphicon glyphicon-th-list'></span> 添加子任务</button>"+"<br/>"
-    );
-    oDiv.append(oP3);
-
-    //deleteBtn
-    var oP4=$("<p class='text-right'></p>");
-    oP4.append("<button type='button' class='btn btn-default btn-sm deleteTask'>" +
-        "<span class='glyphicon glyphicon-th-list'></span> 删除子任务</button>"+"<br/>");
-    oDiv.append(oP4);
     //uploadBtn
     var oP5=$("<p class='text-right'></p>");
-    oP5.append("<button type='button' class='btn btn-default btn-sm upload'>"+
-        "<span class='glyphicon glyphicon-upload'></span> 保存并上传</button>"+"<br/>"
+    oP5.append("<button type='submit' class='btn btn-default btn-sm upload'>"+
+        "<span class='glyphicon glyphicon-upload'></span> 立即发布</button>"+"<br/>"
     );
     oDiv.append(oP5);
     oTask.append(oDiv);
-    //taskBtn-event
-    $(".addTask").click(function(){
-        //findTaskContent-putin
-        var preTask=$(this).parent().siblings(".task");
-
-        //taskDev&&taskStaff
-        preTask.append("<div class='addSon'>"+"<label>子任务</label>"+"<span>"+"</span>"+"</br>"+
-            "<textarea placeholder='任务描述' class='taskDev'></textarea>"+"<br/>"+"<div class='text-right'>"+
-            "<label>任务负责人</label>"+"<input type='text' class='staffName'/>"+"</div>"+"</div>");
-
-    });
-    $(".deleteTask").click(function(){
-        $(".task .addSon:last-child").remove();
-    });
-    $(".upload").click(function(){
-        //获取当前的任务名、任务内容、分任务、分任务负责人都是什么
-        var taskName=$(this).parent().siblings(".taskTitle").find(".taskName");
+    $(".upload").click(function(event) {
+        event.preventDefault();
+        //获取当前的任务名、任务内容、分任务、分任务等级都是什么
+        var projectName = $(".projectName");
+        console.log(projectName.val());
+        var taskName = $(".taskName");
         console.log(taskName.val());
-        var taskContent=$(this).parent().siblings(".task").find(".taskContent");
-        console.log(taskContent.val());
-        var x,y,i,j,flag=true;
-        var taskDev = $(this).parent().siblings(".task").find(".taskDev");
-        var staffName = $(this).parent().siblings(".task").find(".staffName");
-        if(taskName.val()==""||taskContent.val()==""){
-            alert("请填写项目名和任务总述~");
-            flag=false;
-            console.log(flag);
+        var taskDev = $(".taskDev");
+        console.log(taskDev.val());
+        var taskGrade = $(":radio[name='authority']");
+        if (projectName.val() == "") {
+            alert("请填写项目名~");
+
         }
-        for(x in taskDev){
-            if(taskDev[x].value==""){
-                alert("请填写任务描述~");
-                flag=false;
-                console.log(flag);
-            }
+        else if (taskName.val() == "") {
+            alert("请填写任务名");
+
         }
-        for(y in staffName ){
-            if(staffName[y].value==""){
-                alert("请确定任务负责人~");
-                flag=false;
-                console.log(flag);
-            }
+        else if (taskName.val().length > 10) {
+            alert("任务名称请描述在10个字符以内");
+
+
         }
+        else if (taskDev.val() == "") {
+            alert("请填写任务描述~");
 
-
-        if(flag==true){
-            // var taskDev = $(this).parent().siblings(".task").find(".taskDev");
-            var array1 = '';
-            for (i = 0; i < taskDev.length; i++) {
-                array1 += $(taskDev[i]).val() + ",";
-
-            }
-            console.log(array1);
-            // var staffName = $(this).parent().siblings(".task").find(".staffName");
-            var array2 = '';
-            for (j = 0; j < staffName.length; j++) {
-                array2 += $(staffName[j]).val() + ",";
-
-            }
-            console.log(array2);
-            $.ajax({
-                type: 'POST',
-                url: '/addNode',
-                dataType: 'json',
-                data: {
-                    'taskName': taskName.val(),
-                    'taskContent': taskContent.val(),
-                    'taskDev': array1,
-                    'staffName': array2
-                },
-                success: function (data) {
-                    if (parseInt(data) == 1) {
-                        alert("上传成功！");
-                        var oDiv=$(this).parent().parent();
-                        oDiv.hide();
-                    }
-                    else if((parseInt(data) == 0) ) {
-                        alert("上传无效！");
-                    }
-                },
-                error: function () {
-                    alert("请求失败了...");
-                }
-            });
-
+        }
+        else if (taskGrade.is(":checked") == false) {
+            alert("请选择任务等级");
+        }
+        else {
+            $("#task").submit();
         }
     });
+
 
 
 });
