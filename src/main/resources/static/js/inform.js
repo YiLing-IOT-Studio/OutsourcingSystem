42fadfe6bcc5350e0c401070e2d06aa12145c3f9
杨玉卿 <1453582072@qq.com>
2018-04-14 13:14:16 +0800

    yyq 2018.4.14 领取任务

--- src/main/resources/static/js/inform.js
+++ src/main/resources/static/js/inform.js
@@ -2,9 +2,47 @@
  * Created by 杨玉卿 on 2018/4/10.
  */
 
+
 //通知消息
 $("#li_tz_item").click(function(){
-
+    //测试用
+//     // alert("1");
+//     var oDiv=$("#inform");
+//     var clear = "";
+//     oDiv.html(clear);
+//     var oP = $("<div class='op'></div>");
+//
+// //title
+//     var oTitle=$('<p class="oTitle"></p>');
+//     oTitle.append("任务名称："+"前端编写网站");
+//     oP.append(oTitle);
+// //description
+//     var oMsg = $("<p class='description'></p>");
+//     oMsg.append("任务描述："+"前端编写网站前端编写网站前端编写网站前端编写网站前端编写网站前端编写网站");
+//     oP.append(oMsg);
+//     var oUl=$('<ul class="list-inline"></ul>');
+//
+// //author
+//     var oH2 = $("<li><img src='../static/img/emoji" + (parseInt(Math.random() * 5, 10) + 1) + ".png'></li>");
+//     oH2.append('<span class="author">'+''+"杨玉卿"+'</span>');
+//     oUl.append(oH2);
+//
+//
+//     var oDate = $("<li class='day'></li>");
+//     oDate.append("2018/2/5 12:00"+"发布");
+//     oUl.append(oDate);
+//
+//     oP.append(oUl);
+// //btn
+//     var oBtn=$('<div class="float"></div>');
+//     oBtn.append('<button class="btn btn-get">领取任务</button>');
+//     oP.append(oBtn);
+//
+//     oDiv.append(oP);
+//     $(".btn-get").click(function(){
+//         var taskName=$(this).parent().parent().find('.oTitle').text();
+//         alert(taskName);
+//     });
     $.ajax({
         type:"POST",
         url:"/inform",
@@ -78,19 +116,39 @@ $("#li_tz_item").click(function(){
                             }
                         });
                         $(".btn-get").click(function(){
-
+                            var taskName=$(this).parent().parent().find('.oTitle').text();
+                            $.ajax({
+                                type: "POST",
+                                url: "/",
+                                async: false,
+                                dataType: "json",
+                                data: {
+                                    'taskName': taskName
+                                },
+                                success:function(data){
+                                    if(data==1){
+                                        alert("您已经成功领到该任务，请到我的任务栏中查看~");
+                                    }
+                                    else if(data==2){
+                                        alert("正在向管理员申请中...申请成功后将在'我的任务'栏显示您的任务");
+                                    }
+                                },
+                                error:function(){
+                                    alert("发送领取任务请求失败");
+                                }
+                            })
                         });
 
                     },
                     error: function () {
-                        alert("文件上传信息请求失败!");
+                        alert("请求任务信息失败!");
                     }
                 });
             });
 
         },
         error:function(){
-            alert("请求2失败！");
+            alert("请求项目名失败！");
         }
     })
 });
